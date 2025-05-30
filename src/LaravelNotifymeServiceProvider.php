63553a15b4cc96f9a774f4a86e56a5b79810f8be
Yacine Diaf <ycn.diaf@gmail.com>
2025-05-30 14:57:04 +0000

    fix: make the migration file a stub

--- src/LaravelNotifymeServiceProvider.php
+++ src/LaravelNotifymeServiceProvider.php
@@ -6,13 +6,15 @@ use Illuminate\Support\ServiceProvider;
 
 class LaravelNotifymeServiceProvider extends ServiceProvider
 {
-    public function register() {}
+    public function register()
+    {
+    }
 
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/../routes/api.php');
+        $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
         $this->publishesMigrations([
-            __DIR__.'/../database/migrations' => database_path('migrations'),
-        ], 'laravel-notifyme-migrations');
+            __DIR__ . '/../database/migrations' => database_path('migrations'),
+        ], 'migrations');
     }
 }
