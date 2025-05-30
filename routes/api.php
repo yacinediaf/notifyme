20854d3fbffa4ca0f5ee148fee905d8eb037b82a
Yacine Diaf <ycn.diaf@gmail.com>
2025-05-30 14:31:37 +0000

    fix: add the prefix to the api endpoint

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use YacineDiaf\LaravelNotifyme\Controllers\DeviceController;
 
 /**Device Token */
-Route::post('/save_device_token', [DeviceController::class, 'store']);
+Route::prefix('api')->post('/save_device_token', [DeviceController::class, 'store']);
